9c9fe7d4fa230c21b42d191a08f42eeacf6de158
Muhammed DURMAZ <muhammeddurmaz254@gmail.com>
2025-10-07 16:19:39 +0300

    function definitons and variebles adds

--- catch_the_turtle.py
+++ catch_the_turtle.py
@@ -14,6 +14,36 @@ turtle_itself = turtle.Turtle()
 turtle_itself.shape("turtle")
 turtle_itself.speed(0)
 
+#Function to update and display the score board
+def display_the_score_board(score): 
+    turtle_itself.clear()
+    turtle_itself.teleport(0,250)
+    turtle_itself.write(f"Score: {score}", align="center", font=("Courier", 24, "normal"))
+    turtle_itself.teleport(0,0)
+
+def game_scene():
+    turtle_itself.hideturtle()
+    turtle_itself.teleport(randint(-380,380), randint(-280, 280)) 
+    time.sleep(1)
+    turtle_itself.showturtle()
+    time.sleep(1)
+    turtle_itself.onclick(fxn)
+    display_the_score_board(score)
+
+def game_over(i, score):
+    turtle_itself.teleport(0,0)
+    turtle_itself.clear()
+    turtle_itself.write("Game Over", align="center", font=("Courier", 24, "normal"))
+    turtle_itself.teleport(0,-30)
+    turtle_itself.write(f"{score} out of {i+1}", align="center", font=("Courier", 24, "normal"))
+
+#Function to increase score when turtle is clicked
+def fxn(x, y):
+    turtle_itself.hideturtle()
+    global score
+    score +=1
+    print(f"Score: {score}")
+
 
 turtle.mainloop()
 
